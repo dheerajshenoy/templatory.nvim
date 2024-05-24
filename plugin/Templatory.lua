5abddefdbaf9bb710f2b2a3ec5731f2dae322464
Dheeraj Shenoy <dheerajshenoy22@gmail.com>
2024-05-24 08:02:11 +0530

    updated plugin structure

--- plugin/Templatory.lua
+++ plugin/Templatory.lua
@@ -3,7 +3,7 @@ local function command (name, callback, options)
   vim.api.nvim_create_user_command(name, callback, final_opts)
 end
 
-local templatory = require("templatory")
+local templatory = require("Templatory")
 
 command("TemplatoryNew", function () templatory.create_template() end)
 command("TemplatoryVisitFile", function () templatory.visit_file() end)
